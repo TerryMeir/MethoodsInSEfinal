592adb9eca56f2d614610b5eaa9162487b6296bf
TerryMeir <terry.lachmani@gmail.com>
2017-07-27 17:12:59 +0300

    Added main

---
+++ Common/IComposite.cpp
@@ -0,0 +1,25 @@
+#include "IComposite.h"
+
+
+
+IComposite::IComposite()
+{
+}
+
+
+IComposite::~IComposite()
+{
+}
+
+void IComposite::addComponent(IComponent *)
+{
+}
+
+void IComposite::removeComponent(IComponent *)
+{
+}
+
+IComponent * IComposite::getComponent(int)
+{
+	return nullptr;
+}
---
+++ FinalProject/run.cpp
@@ -0,0 +1,13 @@
+//#include "../Common/Graphics.h"
+#include "../Common/EventEngine.h"
+#include <iostream>
+using namespace std;
+void main()
+{
+	//	Graphics g = Graphics();
+	EventEngine program = EventEngine();
+	program.run();
+	//	g.write("hello");
+	int i;
+	cin >> i;
+}
